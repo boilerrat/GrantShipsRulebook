4b77c4b650fc489edc75390929bea073fd8d3858
boilerrat <128boilerrat@gmail.com>
2023-10-29 19:30:25 -0400

    initt

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,17 +2,21 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Grant Ships Rulebook</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://grantships.fun',
   },
   chat: {
     link: 'https://discord.com',
   },
   docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  primaryHue: 260,
+  primarySaturation: 2,
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'Grant Ships Rulebook',
   },
-}
+
+  darkMode: true
+} as any as DocsThemeConfig;
 
 export default config
